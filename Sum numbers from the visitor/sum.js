67df66fa99df3b520e57fdb06934b31baf3652be
Amin Sedghi <sedghi.amin@gmail.com>
2025-09-08 10:40:35 +0330

    Sum numbers from the visitor

---
+++ Sum numbers from the visitor/sum.js
@@ -0,0 +1,5 @@
+let firstNumber = +prompt("pleaser Enter first Number to sum: ", 0);
+
+let seccondNumber = +prompt("pleaser Enter SEccond Number to sum: ", 0);
+
+alert( firstNumber + seccondNumber );
