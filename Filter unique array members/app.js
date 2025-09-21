b4befa8068019871b8fa393089a2d8c0672c8210
Amin Sedghi <sedghi.amin@gmail.com>
2025-09-21 12:51:30 +0330

    use Set collection for optimization of comparison count

--- Filter unique array members/app.js
+++ Filter unique array members/app.js
@@ -1,10 +1,11 @@
 function unique(arr) {
-    let output = [];
+    let output = new Set();
 
     for(let item of arr){
-        if(!output.includes(item)){
+        /* if(!output.includes(item)){
             output.push(item);
-        }
+        } */
+      output.add(item);
     }
 
   return output;
@@ -14,4 +15,7 @@ let strings = ["Hare", "Krishna", "Hare", "Krishna",
   "Krishna", "Krishna", "Hare", "Hare", ":-O"
 ];
 
-alert( unique(strings) ); // Hare, Krishna, :-O
+/* alert( unique(strings) ); // Hare, Krishna, :-O */
+for (let item of unique(strings)){
+  alert(item);
+}
