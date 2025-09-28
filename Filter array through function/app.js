9991a335a0fd9b48a27a28b130635f1de84c0477
Amin Sedghi <sedghi.amin@gmail.com>
2025-09-28 14:07:22 +0330

    Filter array through function

---
+++ Filter array through function/app.js
@@ -0,0 +1,13 @@
+function inBetween(a , b){
+    return (x) => x >= a && a <= b;
+}
+
+function inArray(arr){
+    return (x) => arr.includes(x);
+}
+
+let arr = [1, 2, 3, 4, 5, 6, 7];
+
+alert( arr.filter(inBetween(3, 6)) ); // 3,4,5,6
+
+alert( arr.filter(inArray([1, 2, 10])) ); // 1,2
