d98f47b78293f9e41bd656197d7067d3869deba2
Amin Sedghi <sedghi.amin@gmail.com>
2025-10-05 12:55:37 +0330

    Getters/setters used as wrappers

--- create an accessor with defineProperty/app.js
+++ create an accessor with defineProperty/app.js
@@ -5,14 +5,22 @@ let user = {
 
 Object.defineProperty(user, "fullName", {
     get(){
-        return this.firstName + " "+ this.lastName;
+        return this._firstName + " "+ this._lastName;
     },
 
     set(value){
-        [this.firstName, this.lastName] = value.split(" ");
+        //if we want to forbid too short names for user, we can have a setter name and keep the value in a separate property _name:
+        let arr = value.split(" ");
+        if(arr[0].length < 5){
+            alert("Name is too short...");
+            return;
+        }
+        [this._firstName, this._lastName] = value.split(" ");
     },
 });
 
 user.fullName = "Amin Sedghi";
 
 alert(user.fullName);
+
+user.name = ""; // Name is too short...
