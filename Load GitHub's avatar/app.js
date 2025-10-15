f7754c25ac246e8a4e2985aeb609459eb3aff569
Amin Sedghi <sedghi.amin@gmail.com>
2025-10-15 15:15:59 +0330

    make the chain extendable

--- Load GitHub's avatar/app.js
+++ Load GitHub's avatar/app.js
@@ -3,8 +3,10 @@ fetch(`https://api.github.com/users/sedghiamin`)
   // Load the response as json
   .then(response => response.json())
   // Show the avatar image (githubUser.avatar_url) 
-  .then(function (githubUser){
+  .then(githubUser => new Promise(function (resolve, reject){
     let img = document.createElement('img');
     img.src = githubUser.avatar_url;
     document.body.append(img);
-  });
+    resolve(githubUser);
+  }))
+  .then(githubUser => alert(JSON.stringify(githubUser)));
