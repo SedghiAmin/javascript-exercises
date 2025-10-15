8febfd69882b2dc55bbe4ff90b1db39802acbc94
Amin Sedghi <sedghi.amin@gmail.com>
2025-10-15 15:17:06 +0330

    optimze chaining

--- Promise, Load script/app.js
+++ Promise, Load script/app.js
@@ -14,11 +14,17 @@ function loadScript(src){
 let src = "https://cdnjs.cloudflare.com/ajax/libs/lodash.js/4.17.11/lodash.js";
 let promise = loadScript(src);
 
-promise.then(function (src){ 
-    alert(`${src} is loaded...`);
+promise.then(function (msg){ 
+    alert(`${msg} is loaded...`);
     return loadScript("");
-}).then(function (src){ 
-    alert(`${src} is loaded...`);
-}).catch(
+})
+.then(function (msg){ 
+    alert(`${msg} is loaded...`);
+    return loadScript(src);
+})
+.then(function (msg){ 
+    alert(`${msg} is loaded...`);
+})
+.catch(
     (err) => alert(`we can't load ${err}`),
 );
